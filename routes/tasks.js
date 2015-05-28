11e3290258451bbc25fc048c61c6134cdafc191a
Marion Huebner <mphuebner@gmail.com>
2015-05-28 11:53:47 -0500

    adds CRUD routers

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Task = require('../models/Tasks');
+
+// get /api/tasks
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log(req.body);
+  Task.find(function(error, tasks) {
+    res.json(tasks);
+  });
 });
+
+// GET /api/tasks/:id
+// localhost:5000/api/tasks/0392092-049-2940-2394-2309
+router.get('/:id', function(req, res, next) {
+  Task.findById(req.params.id, function(err, task) {
+    res.json(task);
+  });
+});
+
+// POST /api/tasks
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  Task.create(req.body, function(err, task) {
+    res.json(task);
+  });
+});
+
+// PUT - UPDATE /api/tasks/:id
+router.put('/:id', function(req, res, next){
+  console.log(req.body);
+  Task.findByIdAndUpdate()
+});
+
+// DELETE /api/tasks/:id
+router.delete('/:id', function(req, res, next){
+  Task.findByIdAndRemove(req.params.id, req.body, function(err, task){
+    res.send('Your task has been deleted');
+  })
+})
+
 
 module.exports = router;
